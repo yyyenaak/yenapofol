4b1be572227cb80279adc546940f39d220c308fa
yyyenaak <dpsk185@gmail.com>
2025-02-21 21:38:33 +0900

    js -> usestate 로 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import logo from "../assets/logo.png";
-import "./assets/js/main";
 
 interface HeaderProps {
   menuOpen: boolean;
@@ -8,6 +7,33 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ menuOpen, toggleMenu }) => {
+  const menuRef = useRef<HTMLDivElement>(null);
+  const hamburgerRef = useRef<HTMLDivElement>(null);
+
+  // 바깥 클릭 시 메뉴 닫기
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target as Node) &&
+        hamburgerRef.current &&
+        !hamburgerRef.current.contains(event.target as Node)
+      ) {
+        toggleMenu(); // 메뉴 닫기
+      }
+    };
+
+    if (menuOpen) {
+      document.addEventListener("click", handleClickOutside);
+    } else {
+      document.removeEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [menuOpen, toggleMenu]);
+
   return (
     <header className="header">
       <a href="#top" className="logo">
@@ -17,25 +43,33 @@ const Header: React.FC<HeaderProps> = ({ menuOpen, toggleMenu }) => {
           alt="logo"
         />
       </a>
+
+      {/* 햄버거 버튼 */}
       <div
-        className={`Header_navigation mobile_menu ${menuOpen ? "open" : ""}`}
-        id="mobileMenu"
+        ref={hamburgerRef}
+        className={`hamburger ${menuOpen ? "active" : ""}`}
+        onClick={toggleMenu}
       >
-        <a href="#about" className="navi_font">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* 모바일 메뉴 */}
+      <nav
+        ref={menuRef}
+        className={`Header_navigation mobile_menu ${menuOpen ? "open" : ""}`}
+      >
+        <a href="#about" className="navi_font" onClick={toggleMenu}>
           About
         </a>
-        <a href="#skills" className="navi_font">
+        <a href="#skills" className="navi_font" onClick={toggleMenu}>
           Skills
         </a>
-        <a href="#projects" className="navi_font">
+        <a href="#projects" className="navi_font" onClick={toggleMenu}>
           Projects
         </a>
-      </div>
-      <div className="hamburger" id="hamburger" onClick={toggleMenu}>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
+      </nav>
     </header>
   );
 };
