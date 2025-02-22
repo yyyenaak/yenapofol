e68e810ea97e05c2683f8da48c37f8238f951f67
yyyenaak <dpsk185@gmail.com>
2025-02-22 13:34:38 +0900

    이미지 제외 수정 완료

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,54 +1,92 @@
 import React from "react";
 
-const About: React.FC = () => {
+const About = () => {
   return (
     <section className="component" id="about">
       <div className="mainzone">
-        <div className="main_title flex items-center">
-          <img className="pr-4" src="img/dia.png" alt="dia" />
+        <div className="main_title">
+          <img style={{ paddingRight: "1rem" }} src="img/dia.png" alt="dia" />
           <h1>About</h1>
         </div>
         <div className="aboutzone">
-          <div className="about_row flex flex-col md:flex-row">
-            <div className="about_introduce flex flex-col md:flex-row">
+          <div className="about_row">
+            <div className="about_introduce">
               <div className="about_photo">
                 <img
-                  className="border border-black rounded-full w-80 h-84"
-                  src="src/assets/yyyenaa1.jpg"
+                  style={{
+                    border: "1px solid black",
+                    borderRadius: "100%",
+                    width: "20rem",
+                    height: "21rem",
+                  }}
+                  src="img/yyyenaa1.jpg"
                   alt="Yena Jo"
                 />
               </div>
               <div className="about_col">
                 <div className="about_wrap">
-                  {aboutInfo.map((info, index) => (
-                    <div key={index} className="about_info flex items-center">
+                  {[
+                    {
+                      icon: "people.svg",
+                      title: "NAME",
+                      text: "조예나 (Yena Jo)",
+                    },
+                    { icon: "calender.svg", title: "DoB", text: "2002.01.18" },
+                    {
+                      icon: "location.svg",
+                      title: "LOCATION",
+                      text: "서울시 송파구",
+                    },
+                    {
+                      icon: "phone.svg",
+                      title: "NUMBER",
+                      text: "010-7715-0958",
+                    },
+                    {
+                      icon: "github.svg",
+                      title: "Github",
+                      text: (
+                        <a href="https://github.com/yyyenaak">
+                          github.com/yyyenaak
+                        </a>
+                      ),
+                    },
+                    {
+                      icon: "email.svg",
+                      title: "E-MAIL",
+                      text: "dpsk185@naver.com",
+                      extra: (
+                        <a href="mailto:dpsk185@naver.com">
+                          <img
+                            className="subimg"
+                            src="img/about_svg/send.svg"
+                            alt="send"
+                          />
+                        </a>
+                      ),
+                    },
+                  ].map(({ icon, title, text, extra }, index) => (
+                    <div className="about_info" key={index}>
                       <div className="info_detail">
-                        <img src={info.icon} alt={info.alt} />
+                        <img src={`img/about_svg/${icon}`} alt={title} />
                       </div>
-                      <div className="info_side ml-4">
-                        <div className="info_title font-semibold">
-                          {info.title}
+                      <div className="info_side">
+                        <div className="info_title">
+                          {title}{" "}
+                          {extra && <div className="title_sub">{extra}</div>}
                         </div>
-                        <div className="info_text">
-                          {info.isLink ? (
-                            <a href={info.value} className="text-blue-600">
-                              {info.value}
-                            </a>
-                          ) : (
-                            info.value
-                          )}
-                        </div>
+                        <div className="info_text">{text}</div>
                       </div>
                     </div>
                   ))}
                 </div>
               </div>
             </div>
-            <div className="values mt-6 md:mt-0">
-              <span className="block font-bold">
+            <div className="values">
+              <span>
                 "책임을 다하는 사람만이 변화의 기회를 얻을 수 있습니다."
               </span>
-              <p className="mt-4">
+              <p>
                 책임을 다하는 과정에서 배움을 얻고, 이를 바탕으로 더 큰 도전에
                 나아갈 수 있다고 믿습니다. <br />
                 변화의 순간이 찾아왔을 때 주저하지 않는 것도, 성실히 쌓아온
@@ -63,45 +101,5 @@ const About: React.FC = () => {
     </section>
   );
 };
-
-const aboutInfo = [
-  {
-    icon: "./assets/about_svg/people.svg",
-    alt: "name",
-    title: "NAME",
-    value: "조예나 (Yena Jo)",
-  },
-  {
-    icon: "./assets/about_svg/calender.svg",
-    alt: "Date of Birth",
-    title: "DoB",
-    value: "2002.01.18",
-  },
-  {
-    icon: "./assets/about_svg/location.svg",
-    alt: "Location",
-    title: "LOCATION",
-    value: "서울시 송파구",
-  },
-  {
-    icon: "./assets/about_svg/phone.svg",
-    alt: "phone",
-    title: "NUMBER",
-    value: "010-7715-0958",
-  },
-  {
-    icon: "./assets/about_svg/github.svg",
-    alt: "github",
-    title: "Github",
-    value: "https://github.com/yyyenaak",
-    isLink: true,
-  },
-  {
-    icon: "./assets/about_svg/email.svg",
-    alt: "e-mail",
-    title: "E-MAIL",
-    value: "dpsk185@naver.com",
-  },
-];
 
 export default About;
