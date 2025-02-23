18b2110c2dc3d231f265d45015e87c71f7a82f15
yyyenaak <dpsk185@gmail.com>
2025-02-23 12:59:07 +0900

    map 생성, 이미지 생성 완료

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,51 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.querySelector(".hamburger");
-  const mobileMenu = document.querySelector(".mobile_menu");
   const cards = document.querySelectorAll(".card");
   const skillBacks = document.querySelectorAll(".skill_back");
-
-  // 햄버거 버튼 클릭 시 네비 열기/닫기
-  hamburger.addEventListener("click", () => {
-    if (mobileMenu.classList.contains("active")) {
-      closeMenu();
-    } else {
-      openMenu();
-    }
-  });
-
-  // 네비 열기
-  function openMenu() {
-    hamburger.classList.add("active");
-    mobileMenu.classList.remove("closing");
-    mobileMenu.classList.add("active");
-  }
-
-  // 네비 닫기
-  function closeMenu() {
-    hamburger.classList.remove("active");
-    mobileMenu.classList.remove("active");
-    mobileMenu.classList.add("closing");
-    setTimeout(() => {
-      mobileMenu.classList.remove("closing");
-    }, 300);
-  }
-
-  // 메뉴 외부 클릭 시 메뉴 닫기
-  document.addEventListener("click", (event) => {
-    const isClickInside =
-      hamburger.contains(event.target) || mobileMenu.contains(event.target);
-    if (!isClickInside) {
-      closeMenu();
-    }
-  });
-
-  // 메뉴 항목 클릭 시 메뉴 닫기
-  const menuItems = mobileMenu.querySelectorAll(".navi_font");
-  menuItems.forEach((item) => {
-    item.addEventListener("click", () => {
-      closeMenu();
-    });
-  });
 
   // skill 모바일 여닫기(클릭 시 아래로 펼쳐지게)
   skillBacks.forEach((skillBack) => {
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,24 +1,133 @@
 import React from "react";
+import skills from "../assets/skill.svg";
+import html5 from "../assets/skill_svg/html5.svg";
+import css3 from "../assets/skill_svg/css3.svg";
+import scss from "../assets/skill_svg/scss.svg";
+import react from "../assets/skill_svg/react.svg";
+import git from "../assets/skill_svg/git.svg";
+import figma from "../assets/skill_svg/figma.svg";
+import java from "../assets/skill_svg/java.svg";
+import mysql from "../assets/skill_svg/mysql.svg";
+import php from "../assets/skill_svg/php.svg";
+import js from "../assets/skill_svg/js.svg";
+import tsx from "../assets/skill_svg/tsx.svg";
+import python from "../assets/skill_svg/python.svg";
+import "../assets/js/main";
 
 const Skills: React.FC = () => {
   return (
     <section className="component" id="skills">
       <div className="mainzone">
         <div className="main_title">
+          <img style={{ paddingRight: "1rem" }} src={skills} alt="Skills" />
           <h1>Skills</h1>
         </div>
+
         <div className="skillzone">
+          {/** Familiar Skills **/}
           <div className="skill_back">
-            <h2>Familier</h2>
-            <p>HTML, CSS, SCSS, React, Git, Figma</p>
+            <div className="skill_header"></div>
+            <div className="sk_title">
+              <h2>Familiar</h2>
+            </div>
+            <hr style={{ width: "100%", color: "#8a8a8a" }} />
+            <div className="skill_box">
+              <ul className="sk_detail">
+                {[
+                  {
+                    src: html5,
+                    alt: "HTML5",
+                    name: "HTML",
+                  },
+                  { src: css3, alt: "CSS3", name: "CSS" },
+                  { src: scss, alt: "SCSS", name: "SCSS" },
+                  {
+                    src: react,
+                    alt: "React",
+                    name: "React",
+                  },
+                  { src: git, alt: "Git", name: "Git" },
+                  {
+                    src: figma,
+                    alt: "Figma",
+                    name: "Figma",
+                  },
+                ].map((skill, index) => (
+                  <li key={index}>
+                    <div className="logobox">
+                      <img src={skill.src} alt={skill.alt} />
+                    </div>
+                    <span>{skill.name}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
+
+          {/** Tried Skills **/}
           <div className="skill_back">
-            <h2>Tried</h2>
-            <p>Java, MySQL, PHP</p>
+            <div className="skill_header"></div>
+            <div className="sk_title">
+              <h2>Tried</h2>
+            </div>
+            <hr style={{ width: "100%", color: "#8a8a8a" }} />
+            <div className="skill_box">
+              <ul className="sk_detail">
+                {[
+                  { src: java, alt: "Java", name: "Java" },
+                  {
+                    src: mysql,
+                    alt: "MySQL",
+                    name: "MySQL",
+                  },
+                  { src: php, alt: "PHP", name: "PHP" },
+                ].map((skill, index) => (
+                  <li key={index}>
+                    <div className="logobox">
+                      <img src={skill.src} alt={skill.alt} />
+                    </div>
+                    <span>{skill.name}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
+
+          {/** Language Skills **/}
           <div className="skill_back">
-            <h2>Languages</h2>
-            <p>JavaScript, TypeScript, Python</p>
+            <div className="skill_header"></div>
+            <div className="sk_title">
+              <h2>Language</h2>
+            </div>
+            <hr style={{ width: "100%", color: "#8a8a8a" }} />
+            <div className="skill_box">
+              <ul className="sk_detail">
+                {[
+                  {
+                    src: js,
+                    alt: "JavaScript",
+                    name: "JavaScript",
+                  },
+                  {
+                    src: tsx,
+                    alt: "TypeScript",
+                    name: "TypeScript",
+                  },
+                  {
+                    src: python,
+                    alt: "Python",
+                    name: "Python",
+                  },
+                ].map((skill, index) => (
+                  <li key={index}>
+                    <div className="logobox">
+                      <img src={skill.src} alt={skill.alt} />
+                    </div>
+                    <span>{skill.name}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
